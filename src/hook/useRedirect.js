68f896b031fa93844deec0797faf612548c4c002
luka <lukabarbakadze18@gmail.com>
2023-04-01 09:59:27 +0400

    update

--- src/hook/useRedirect.js
+++ src/hook/useRedirect.js
@@ -12,30 +12,7 @@ export default function useRedirect(path) {
     const redirectUser = async () => {
       const user = JSON.parse(localStorage.getItem("user"));      
       const isLoggedIn = user !== null && user !== "null" && user !== "undefined" && user.hasOwnProperty('token');
-      // let isLoggedIn = false;
-
-
-      // fetch("http://localhost:5000/api/auth/loggedinstatus", {
-      //   headers: {
-      //     Authorization: `Bearer ${
-      //       localStorage.getItem("user") &&
-      //       JSON.parse(localStorage.getItem("user")).token
-      //     }`,
-      //   },
-      // })
-      //   .then((response) => {
-      //     if (!response.ok) {
-      //       throw new Error("Network response was not ok");
-      //     }
-      //     return response.json();
-      //   })
-      //   .then((data) => {
-      //     isLoggedIn = data;
-      //   })
-      //   .catch((error) => {
-      //     console.error("There was a problem with the fetch request:", error);
-      //   });
-
+      
       console.log("isLoggedIn:", isLoggedIn);
       dispatch(setLogin(isLoggedIn));
 
